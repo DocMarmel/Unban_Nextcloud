736c603373afb34e77cc1743e358d44f2a568f58
Maxime <33199920+DocMarmel@users.noreply.github.com>
2019-07-15 17:34:46 +0200

    Update PageController.php

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -3,7 +3,6 @@ namespace OCA\Unban\Controller;
 
 use OCP\IRequest;
 use OCP\AppFramework\Http\TemplateResponse;
-use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Controller;
 
 class PageController extends Controller {
